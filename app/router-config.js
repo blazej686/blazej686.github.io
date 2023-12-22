409d653d1d3415eb3e818c4b5153661938221763
code . <blazej686@yahoo.com>
2023-12-21 17:53:25 -0700

    landing page is live and coming soon

--- app/router-config.js
+++ app/router-config.js
@@ -1,4 +1,3 @@
-import { ExamplesController } from "./controllers/ExamplesController.js";
 import { HomeController } from "./controllers/HomeController.js";
 import { Router } from "./utils/Router.js";
 
@@ -6,15 +5,8 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
   {
     path: '',
-    controllers: [HomeController, ExamplesController],
-    view: /*html*/`
-    <div class="bg-white p-3">
-      <div class="card-body">
-        <p>Home Page</p>
-        <button class="btn btn-dark" onclick="app.HomeController.testButton()">😎</button>
-      </div>
-    </div>
-    `
+    controllers: [HomeController],
+    view: 'app/views/HomePage.html'
   },
   {
     path: '#/about',
